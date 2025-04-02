0331635f472dd108bb05930e4419db5f38954c7f
Sara Eisazadeh <101432538@georgebrown.ca>
2025-04-02 19:55:49 -0400

    Finalizing the LabTest

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'mission/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Example: You can dynamically fetch this from the API
+      const response = await fetch('https://api.spacexdata.com/v3/launches');
+      const launches = await response.json();
+
+      return launches.map((launch: any) => ({
+        id: launch.flight_number.toString()
+      }));
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
